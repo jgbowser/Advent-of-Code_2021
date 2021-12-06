df75cac820df97eb322149239bd10951abd7e828
John <john.bow7720@gmail.com>
2021-12-05 20:19:34 -0700

    day 3, part 2

--- Day3/binaryDiagnostics.js
+++ Day3/binaryDiagnostics.js
@@ -2,8 +2,10 @@ const fs = require('fs');
 
 // Part 1: convert binary diagnostics to gamma and epsilon rates, then get total power consumption
 const rawDiagnostics = fs.readFileSync("./diagnostics.txt", "utf-8");
+// format raw data into an array, ensure there are no empty values
 const formattedDiagnostics = rawDiagnostics.split("\n").filter(d => !!d);
 
+// get the number of digits in a diagnostic code
 const digits = formattedDiagnostics[0].length;
 
 function getGammaRate(diagnostics) {
@@ -48,4 +50,49 @@ function getEpsilonRate(gamma) {
 const gammaRate = getGammaRate(formattedDiagnostics);
 const epsilonRate = getEpsilonRate(gammaRate);
 const powerConsumption = parseInt(gammaRate, 2) * parseInt(epsilonRate, 2);
-console.log(powerConsumption);
+console.log("Power consumption: ", powerConsumption);
+
+// Part 2: Oxygen rating and CO2 Scrubber rating
+
+function getFilterBit(data, currentBit, rating) {
+  let ones = 0;
+  let zeros = 0;
+  let filterBit;
+  for (let i = 0; i < data.length; i++) {
+    if (data[i][currentBit] === "1") {
+      ones++
+    } else {
+      zeros++
+    }
+  }
+  // Filter bit for oxygen is most common value, or 1 if equal
+  if (rating === "O2") {
+    filterBit = zeros === ones ? "1" : zeros > ones ? "0" : "1";
+  }
+  // Filter bit for CO2 is least common value, or 0 if equal
+  if (rating === "CO2") {
+    filterBit = zeros === ones ? "0" : zeros > ones ? "1" : "0";
+  }
+
+  return filterBit;
+}
+
+function getSystemRating(diagnostics, rating) {
+  let filteredValues = diagnostics;
+  let currentBit = 0;
+
+  while (filteredValues.length > 1) {
+    let filterBit = getFilterBit(filteredValues, currentBit, rating);
+    filteredValues = filteredValues.filter(value => value[currentBit] === filterBit);
+    currentBit++
+  }
+
+  return filteredValues[0];
+}
+
+const O2Rating = getSystemRating(formattedDiagnostics, "O2");
+const CO2Rating = getSystemRating(formattedDiagnostics, "CO2");
+
+const lifeSupportRating = parseInt(O2Rating, 2) * parseInt(CO2Rating, 2);
+
+console.log("Life support rating: ", lifeSupportRating);
