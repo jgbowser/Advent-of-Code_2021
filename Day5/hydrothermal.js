2ac7ae6e35a2d1369c9beb812b3046af67b6ca73
John <john.bow7720@gmail.com>
2021-12-09 00:47:34 -0700

    day 5, part 1

--- Day5/hydrothermal.js
+++ Day5/hydrothermal.js
@@ -1,6 +1,6 @@
 const fs = require("fs");
 
-const data = fs.readFileSync("./test.txt", "utf-8");
+const data = fs.readFileSync("./ventPlot.txt", "utf-8");
 
 const plotPoints = data
   .split("\n")
@@ -22,19 +22,87 @@ function filterOutDiagonals(allPoints) {
   });
 };
 
-function interpolateLines(points) {
+function interpolateHorizontalLines(points, start, end, constant) {
+  const line = [points[0], points[1]];
+
+  if(start < end) {
+    for(let i = start + 1; i < end; i++) {
+      line.push(`${i},${constant}`);
+    };
+  } else {
+    for(let i = start - 1; i > end; i--) {
+      line.push(`${i},${constant}`)
+    }
+  }
+
+  return line;
+};
+
+function interpolateVerticalLines(points, start, end, constant) {
+  const line = [points[0], points[1]];
+
+  if(start < end) {
+    for(let i = start + 1; i < end; i++) {
+      line.push(`${constant},${i}`);
+    };
+  } else {
+    for(let i = start - 1; i > end; i--) {
+      line.push(`${constant},${i}`)
+    }
+  }
+
+  return line;
+};
+
+function interpolateLines(allPoints) {
   // sort each set of points in ascending order to make things a bit more manageable
-  points.sort();
-  const interpolatedLine = [];
-  const allLines =[];
-  // for each set of points first check if it is changing horizontally or vertically
-  // then parseInt the start and end numbers
-  // for loop and push the interpolated points from i = 1 + start; i < end -> .push(`${i},{constant}`)
-  // once the loop ends, push the whole interpolated set into allLines and repeat
+  allPoints.sort();
+  const interpolatedLines = [];
+
+  allPoints.forEach(set => {
+    const x1 = parseInt(set[0].split(",")[0]);
+    const x2 = parseInt(set[1].split(",")[0]);
+    const y1 = parseInt(set[0].split(",")[1]);
+    const y2 = parseInt(set[1].split(",")[1]);
+
+    // for each set of points first check if it is changing horizontally or vertically
+    if (x1 === x2) {
+      interpolatedLines.push(interpolateVerticalLines(set, y1, y2, x1));
+    }
+
+    if(y1 === y2) {
+      interpolatedLines.push(interpolateHorizontalLines(set, x1, x2, y1));
+    }
+  })
   // once we have all interpolated lines, flatten the array and loop through incrementing counter any time we see a duplicate for the first time
   // return the value of the counter for our final answer
 
+  return interpolatedLines;
+};
+
+function countDangerZones(data) {
+  let count = 0;
+  let lastCountedDupe;
+  let comparisonValue;
+
+  for(let i = 0; i < data.length; i++) {
+    // if we have already counted this point as a having a duplicate skip this iteration
+    if(comparisonValue === data[i] && lastCountedDupe === data[i]) continue;
+    // if this point equals the comparisonValue, but we haven't counted it as a dupe increment count and set lastCountedDupe to this value
+    if(comparisonValue === data[i] && lastCountedDupe !== data[i]) {
+      count++
+      lastCountedDupe = data[i];
+    };
+    // if this point does not equal the comparisonValue it isn't a duplicate, so set the comparison and move along
+    if(comparisonValue !== data[i]) {
+      comparisonValue = data[i]
+    };
+  };
+
+  return count;
 };
 
 const filteredPoints = filterOutDiagonals(plotPoints);
 const interpolatedData = interpolateLines(filteredPoints);
+const flattenedSortedData = interpolatedData.flat().sort();
+console.log(countDangerZones(flattenedSortedData));
